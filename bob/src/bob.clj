533e6d8989649730ccb35c27ae2445f7f01d94b8
query-set <j3k.walczak@gmail.com>
2020-11-05 23:50:38 +0100

    WIP on Bob

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,6 +1,13 @@
-(ns bob)
+(ns bob
+  :require [clojure.string :as str])
 
 (defn response-for
   [s]
-  s
-)
+  (let [n-s (str/trimr s)]
+    (case n-s
+      (empty? n-s) "Fine. Be that way!"
+      (upper? n-s) (if (= (last s) \?)
+                     "Calm down, I know what I'm doing!"
+                     "Whoa, chill out!")
+      (= (last s) \?) "Sure."
+      "Whatever.")))
