5469b2ff13e108bc7c3e067b223518a838928f7f
query-set <j3k.walczak@gmail.com>
2020-11-10 09:42:25 +0100

    2nd iteration of Hamming: transform `comp` to inline function

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -1,13 +1,8 @@
 (ns hamming)
 
-(defn comp
-  [s1 s2]
-  (if (not= s1 s2)
-    1
-    0))
-
 (defn distance
   [strand1 strand2]
   (when (= (count strand1) (count strand2))
-    (reduce + (map comp strand1 strand2)))
-)
+    (reduce + (map
+             (fn [s1 s2] (if (not= s1 s2) 1 0))
+             strand1 strand2))))
