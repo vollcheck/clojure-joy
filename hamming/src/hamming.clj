539580dc4e3d94f608fda53a21069313df5ed3f4
query-set <j3k.walczak@gmail.com>
2020-11-11 17:43:08 +0100

    Hamming improved

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -4,5 +4,5 @@
   [strand1 strand2]
   (when (= (count strand1) (count strand2))
     (reduce + (map
-             (fn [s1 s2] (if (not= s1 s2) 1 0))
+             #(if (not= %1 %2) 1 0)
              strand1 strand2))))
