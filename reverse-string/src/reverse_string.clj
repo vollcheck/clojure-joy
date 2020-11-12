3b624e9b21d8348179b322633c39b43beecc2c6b
query-set <j3k.walczak@gmail.com>
2020-11-12 09:57:33 +0100

    Improved reverse-string solution using more abstraction

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,14 +1,6 @@
 (ns reverse-string)
 
-(defn my-reverse
-  "Auxiliary function for reversing."
-  [s result-s]
-  (if (= 0 (count s))
-    result-s
-    (my-reverse (rest s) (str (first s) result-s))))
-
-
 (defn reverse-string
   "Function that returns reversed string."
   [s]
-  (my-reverse s ""))
+  (apply str (reduce conj () s)))
